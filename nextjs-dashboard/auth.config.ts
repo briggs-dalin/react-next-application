ba432b2c6eae5673dbdf9aa1c63b46b2a2bd652f
= <briggs.dalin09@gmail.com>
2025-09-17 21:28:54 -0600

    I'm not sure anymore

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,20 +1,21 @@
 import type { NextAuthConfig } from 'next-auth';
-
+ 
 export const authConfig = {
-  pages: { signIn: '/login' },
+  pages: {
+    signIn: '/login',
+  },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isDashboard = nextUrl.pathname.startsWith('/dashboard');
-
-      if (isDashboard) {
-        return isLoggedIn;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
-
       return true;
     },
   },
-  providers: [], // Providers added in auth.ts
+  providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
